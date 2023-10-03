937cf095e5177fb417091fa3d387722bdc577802
Pol Pere Rivero García <pori9503@proven.local>
2023-10-03 11:23:50 +0200

    salario sin impuestos

--- UF1-NF1(IF-BUCLES)/exercici_18_salari/src/exercici_18_salari/Exercici_18_salari.java
+++ UF1-NF1(IF-BUCLES)/exercici_18_salari/src/exercici_18_salari/Exercici_18_salari.java
@@ -16,15 +16,32 @@ public class Exercici_18_salari {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int tarifa_normal, horas_normal, horas;
-        double salario, salario_extra;
-        
-        tarifa_normal = 20;
-        horas_normal = 35;
-        salario = tarifa_normal * horas_normal;
+        double T_NORMAL, HORAS_NORMAL, h_extra, salario_extra, salario_normal, salario;
+        int horas;
         Scanner sc = new Scanner(System.in);
         System.out.println("Cuantas horas has trabajado?");
         horas = sc.nextInt();
+        
+        T_NORMAL = 20;
+        HORAS_NORMAL = 35;
+        
+
+        
+        if (horas>HORAS_NORMAL)
+        {
+        h_extra = horas - HORAS_NORMAL;
+        salario_extra = h_extra*T_NORMAL*1.5;
+        salario_normal = HORAS_NORMAL * T_NORMAL;
+        salario = salario_extra + salario_normal;
+            
+        }
+        else
+        {
+            salario = horas * T_NORMAL;
+            
+            
+        }
+        System.out.println("Tu salario es " + salario);
         
     }
     
