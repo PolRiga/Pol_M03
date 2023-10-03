66632479cd706c2588f88df1ffd77d7e2ea6dc7e
Pol Pere Rivero García <pori9503@proven.local>
2023-10-03 12:56:45 +0200

    Salario con impuestos

--- UF1-NF1(IF-BUCLES)/exercici_18_salari/src/exercici_18_salari/Exercici_18_salari.java
+++ UF1-NF1(IF-BUCLES)/exercici_18_salari/src/exercici_18_salari/Exercici_18_salari.java
@@ -16,8 +16,11 @@ public class Exercici_18_salari {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        double T_NORMAL, HORAS_NORMAL, h_extra, salario_extra, salario_normal, salario;
+        double T_NORMAL, HORAS_NORMAL,impuestos, salario_impuestos, IMPUESTOS_25;
+        double h_extra, salario_extra, salario_normal, salario, salario_tope;
         int horas;
+        final int SALARIO_SIN_IMPUESTOS = 500;
+        final int SALARIO_TOPE_IMPUESTOS = 900;
         Scanner sc = new Scanner(System.in);
         System.out.println("Cuantas horas has trabajado?");
         horas = sc.nextInt();
@@ -41,7 +44,29 @@ public class Exercici_18_salari {
             
             
         }
-        System.out.println("Tu salario es " + salario);
+        System.out.println("Tu salario BRUTO es " + salario);
+        
+       if (salario>SALARIO_TOPE_IMPUESTOS)
+       {
+       salario_tope = salario - SALARIO_TOPE_IMPUESTOS;
+       impuestos = salario_tope*0.45;
+       salario_impuestos = SALARIO_TOPE_IMPUESTOS - SALARIO_SIN_IMPUESTOS;
+       IMPUESTOS_25 = salario_impuestos * 0.25;
+       impuestos = impuestos + IMPUESTOS_25;
+       }
+       
+       else if(salario>SALARIO_SIN_IMPUESTOS && salario < SALARIO_TOPE_IMPUESTOS)
+         {
+                impuestos = salario*0.25;
+                salario = salario - impuestos;
+                System.out.println("Tu salario NETO es " + salario);
+                System.out.println("Los impuestos son " + impuestos);
+         }
+        /*else
+        {
+            System.out.println("Tu salario es " + salario);
+        }   
+        */
         
     }
     
